579d498fc5b1dd8067b105f4407854e48a716169
SRP-STD\j.brosset <jordane.brosset@showroomprive.net>
2025-05-28 10:06:17 +0200

    fix: js extensions

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <SpeedInsights />
-            <Analytics />
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <SpeedInsights />
+      <Analytics />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
