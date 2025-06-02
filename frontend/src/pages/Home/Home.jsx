36ceb233dd855bf64c00e21a0e2c660d5570f9cf
SRP-STD\j.brosset <jordane.brosset@showroomprive.net>
2025-06-02 15:42:27 +0200

    fix: seo

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
                     <h1>Jordane Brosset</h1>
                     <h2>{t("title")}</h2>
                 </div>
-                <img src={"static/img/profilePicture.webp"} alt="Profile Picture" className="profilePicture"></img>
+                <img src={"static/img/profilePicture.webp"} alt="Profile Picture" className="profilePicture"/>
             </div>
         </motion.div>
     );
