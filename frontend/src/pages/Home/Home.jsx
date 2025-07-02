5b1a7944792ba51f8241193e1f8255b3f6e45ea0
Jordane Brosset <jordane.brosset@gmail.com>
2025-07-02 10:18:56 +0200

    fix: update color variables and improve image styles in SCSS files

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { pageStyle } from "../../utils/transition_variants";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 export default function Home() {
-    const {t} = useTranslation()
+    const { t } = useTranslation();
     return (
         <motion.div className="mainHome" style={pageStyle} initial="initial" animate="in" exit="out" variants={pageVariants} transition={pageTransition}>
             <div className="homeContent">
@@ -13,7 +13,7 @@ export default function Home() {
                     <h1>Jordane Brosset</h1>
                     <h2>{t("title")}</h2>
                 </div>
-                <img src={"static/img/profilePicture.webp"} alt="Profile Picture" className="profilePicture"/>
+                <img src={"static/img/profilePicture.webp"} alt="Profile Picture" className="profilePicture" />
             </div>
         </motion.div>
     );
